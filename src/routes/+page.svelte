76e2182dcc088b4b8ba41780d072da60c4595f3b
GamerC0der <159653318+GamerC0der@users.noreply.github.com>
2025-11-10 14:38:40 -0800

    Make Svelte App

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<h1>Super Meeple Galaxy</h1>
+
+<style>
+	:global(body) {
+		background-color: black;
+		color: white;
+		margin: 0;
+		padding: 0;
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		font-family: system-ui, -apple-system, sans-serif;
+	}
+
+	h1 {
+		font-size: 3rem;
+		margin: 0;
+	}
+</style>
